ee75fb5909b2fc77df8bdf2722b6c74e4a725a73
Paul <paul@drurys.org>
2018-01-04 11:31:30 +0000

    force paste as plain text excpet when in admin

--- src/functions.php
+++ src/functions.php
@@ -33,3 +33,45 @@ function pd_default_menu() {
     $html .= pd_make_menu_option('cookies', 'Cookies');
 	echo $html;
 } 
+
+class PeeDeePasteAsPlainText {
+
+        function __construct() {
+            add_action( 'init', array( $this, 'init' ) );
+        }
+
+        function init() {
+            add_filter( 'tiny_mce_before_init', array( $this, 'force_paste_as_plain_text' ) );
+            add_filter( 'teeny_mce_before_init', array( $this, 'force_paste_as_plain_text' ) );
+            add_filter( 'teeny_mce_plugins', array( $this, 'load_paste_plugin' ) );
+            add_filter( 'mce_buttons_2', array( $this, 'remove_button' ) );
+        }
+
+        function force_paste_as_plain_text( $mceInit ) {
+            global $tinymce_version;
+
+            if ( $tinymce_version[0] < 4 ) {
+                    $mceInit[ 'paste_text_sticky' ] = true;
+                    $mceInit[ 'paste_text_sticky_default' ] = true;
+            } else {
+                    $mceInit[ 'paste_as_text' ] = true;
+            }
+            return $mceInit;
+        }
+
+        function load_paste_plugin( $plugins ) {
+            return array_merge( $plugins, array( 'paste' ) );
+        }
+
+        function remove_button( $buttons ) {
+            if( ( $key = array_search( 'pastetext', $buttons ) ) !== false ) {
+                    unset( $buttons[ $key ] );
+            }
+            return $buttons;
+        }
+
+}
+
+if ( !is_admin() ) {
+    new PeeDeePasteAsPlainText();
+}
